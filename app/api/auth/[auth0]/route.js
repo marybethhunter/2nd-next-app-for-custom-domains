5bc36007211d5d6ef92927f91a6286834c6bd747
Mary Beth Hunter <marybeth.hunter@okta.com>
2024-01-19 17:45:19 -0600

    hardcode redirect_uri

--- app/api/auth/[auth0]/route.js
+++ app/api/auth/[auth0]/route.js
@@ -2,10 +2,11 @@ import { handleAuth, handleLogin  } from '@auth0/nextjs-auth0';
 
 export const GET = handleAuth({
     login: handleLogin({
-        // authorizationParams: {
-        //     audience: process.env.AUTH0_AUDIENCE,
-        //     scope: 'openid profile email read:shows',
-        // },
+        authorizationParams: {
+            // audience: process.env.AUTH0_AUDIENCE,
+            // scope: 'openid profile email read:shows',
+            redirect_uri: 'https://2nd-next-app-for-custom-domains-rdaw1dh44-marybethhunter.vercel.app/'
+        },
         returnTo: '/profile'
     })
 });
